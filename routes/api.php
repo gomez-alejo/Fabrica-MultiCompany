d97462def9a6f8001cc1591da235ee2aac78d8be
miguelcamilok <miguelcamilok@gmail.com>
2025-07-01 01:06:25 -0500

    fix: corrected controller references in InvoiceProduct routes

--- routes/api.php
+++ routes/api.php
@@ -10,15 +10,15 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::prefix('InvoicesProducts')->group(function(){
-    Route::get('/', [ProductRequestController::class, 'index']);
-    Route::post('/', [ProductRequestController::class, 'store']);
-    Route::get('{id}', [ProductRequestController::class, 'show']);
-    Route::put('{id}', [ProductRequestController::class, 'update']);
-    Route::delete('{id}', [ProductRequestController::class, 'destroy']);
+Route::prefix('invoiceProducts')->group(function(){
+    Route::get('/', [InvoiceProductController::class, 'index']);
+    Route::post('/', [InvoiceProductController::class, 'store']);
+    Route::get('{id}', [InvoiceProductController::class, 'show']);
+    Route::put('{id}', [InvoiceProductController::class, 'update']);
+    Route::delete('{id}', [InvoiceProductController::class, 'destroy']);
 });
 
-Route::prefix('ProductRequests')->group(function(){
+Route::prefix('productRequests')->group(function(){
     Route::get('/', [ProductRequestController::class, 'index']);
     Route::post('/', [ProductRequestController::class, 'store']);
     Route::get('{id}', [ProductRequestController::class, 'show']);
